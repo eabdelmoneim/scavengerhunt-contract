8779612d22ea00f69298fd55d11ffccc73a775fa
Eiman <eabdelmoneim@gmail.com>
2023-04-10 23:00:38 -0500

    add swapping capabilities

--- src/ScavengerHunt1155Drop.sol
+++ src/ScavengerHunt1155Drop.sol
@@ -8,6 +8,18 @@ contract ScavengerHunt1155Drop is ERC1155LazyMint {
     uint256[] winningTokenIds;
     bool public isHuntPaused = false;
 
+    struct SwapOffer {
+        address from;
+        uint256 id;
+        uint256 forId;
+    }
+
+    mapping(address => SwapOffer) public offers;
+
+    event SwapOfferCreated(address indexed from, uint256 indexed tokenId, uint256 indexed forTokenId);
+    event SwapOfferCancelled(address indexed from, uint256 indexed tokenId, uint256 indexed forTokenId);
+    event SwapTrade(address indexed from, address indexed to);
+
     constructor(
         string memory _name,
         string memory _symbol
@@ -75,5 +87,73 @@ contract ScavengerHunt1155Drop is ERC1155LazyMint {
     function stopHunt() external {
         require(msg.sender == owner(), "Only owner can stop the scavenger hunt");
         isHuntPaused = true;
+    }
+
+// Swap functions
+    
+    /**
+     * @notice Creates a swap offer for a token
+     */
+    function createSwapOffer(address _from, uint256 _tokenId, uint256 _forTokenId) public {
+        require(_from == msg.sender, "Only the owner can create an offer.");
+        require(balanceOf[_from][_tokenId] >= 1, "Insufficient balance.");
+        
+        // require that the token id is not the last token id
+        require(_forTokenId < nextTokenIdToMint()-1, "Invalid token id to swap for.");
+
+        // require that the msg.sender doesn't already have an offer
+        require(offers[msg.sender].from == address(0), "An address can only have one offer at a time.");
+        
+        offers[msg.sender] = SwapOffer(_from, _tokenId, _forTokenId);
+
+        emit SwapOfferCreated(_from, _tokenId, _forTokenId);
+    }
+
+    /**
+     * @notice Cancels a swap offer for a token
+     */
+    function cancelSwapOffer() public {
+        require(offers[msg.sender].from != address(0), "No offer to cancel.");
+
+        uint256 id = offers[msg.sender].id;
+        uint256 forId = offers[msg.sender].forId;
+
+        delete offers[msg.sender];
+
+        emit SwapOfferCancelled(msg.sender, id, forId);
+    }
+
+    /**
+     * @notice returns the swap offer for an address
+     */    
+    function getSwapOffer(address _from) public view returns (SwapOffer memory) {
+        return offers[_from];
+    }
+
+    /**
+     * @notice conducts a swap between two addresses that have open swap offers
+     */
+    function swap(address _from, address _to) public {
+        SwapOffer memory offer1 = offers[_from];
+        SwapOffer memory offer2 = offers[_to];
+
+        require(offer1.from != address(0), "No open swap offers found for address 1.");
+        require(offer2.from != address(0), "No open swap offers found for address 2.");
+        require(offer1.from != _to, "Can't trade with yourself.");
+        require(offer1.id != offer2.id, "Can't swap the same token.");
+        require(offer1.forId == offer2.id && offer2.forId == offer1.id, "Swap offers don't match.");
+
+        require(balanceOf[_from][offer1.id] >= 1, "Insufficient balance of token for from address.");
+        require(balanceOf[_to][ offer2.id] >= 1, "Insufficient balance of token for to address");
+
+        // perform the swap
+        _safeTransferFrom(_from, _to, offer1.id, 1, "");
+        _safeTransferFrom(_to, _from, offer2.id, 1, "");
+
+        // delete the offers
+        delete offers[_from];
+        delete offers[_to];
+
+        emit SwapTrade(_from, _to);
     }
 }
